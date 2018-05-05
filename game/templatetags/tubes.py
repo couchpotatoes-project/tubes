9ee39379fa88ddfbf110c160290c133697ddf7de
= <=dave.heena05@gmail.com>
2018-05-04 20:30:58 -0400

    Adding changes to show UserScore in Admin

--- game/templatetags/tubes.py
+++ game/templatetags/tubes.py
@@ -5,28 +5,12 @@ Created on Wed Sep 27 20:55:35 2017
 @author: Heena
 """
 from django import template
+from django.template.defaulttags import register
 ##from django.shortcuts import tubes, render
 
 register = template.Library()
 
 
-@register.simple_tag
-def squares(value):
-    squares = value
-    return squares
-
-
-@register.simple_tag
-def points(values):
-    points = [1,4,5,8,9,12,13,16]
-    return {points: points}
-	
-@register.simple_tag
-def count():
-    iterator=itertools.count()
-    return iterator
-	
-
-def tubes(request, question_id):    
-	result = [4,1,4,5,8,9,12,13,16]
-	return render(request, 'tubes.html', {'rows': result})
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
